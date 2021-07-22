2261f2c8fd8d37a63d6cdd1821d2ca60add81ecd
domenick-deluca <36242411+domenick-deluca@users.noreply.github.com>
2021-07-22 16:37:39 -0700

    Implemented takeMove function
    Combined functionality of checkLegalMove and checkIntermediatePieces to write a function that checks legality/possibility of a given move, then takes that move and returns the resulting board state as well as piece if captured.

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def makeBoard(type):
 # piece ~ tuple giving array position (NOT location on board) of piece to move
 # pos ~ tuple giving desired location on board (NOT array position) to move piece to
 def checkIntermediatePieces(board, piece, pos):
-    array_pos = [(5-pos[0]), (pos[1]-1)] # Tuple of location in array of desired space to move to. Array pos ends up being [y, x]. Confusing, I know...
+    array_pos = [(5-pos[0]), (pos[1]-1)] # Tuple of location in array of desired space to move to.
     # Check for piece type; rook, bishop, and lance are the only ones we care about.
     pieces = ["r", "b", "l"]
     if board[piece[0]][piece[1]].pieceType in pieces:
@@ -98,9 +98,43 @@ def checkIntermediatePieces(board, piece, pos):
         else:
             return True
 
-# Completes a given move...
+# Completes a given move. Checks for legality and possibility before doing so.
+# Args are same as checkIntermediatePieces.
+# Returns a tuple:
+# [0] is False if not a legal or possible move.
+#     - if False, [1] is 0 if illegal, 1 if intervening pieces found.
+# [0] is True if move has been made.
+#     - if True, [1] is the resulting board state after making given move. [2] is a piece if captured, None if not.
 def takeMove(board, piece, pos):
-    return None
+    # Check legality of move for specific piece.
+    legal = board[piece[0]][piece[1]].checkLegalMove(pos)
+    if not legal:
+        return [False, 0]
+    # Check possibility of move for piece on board.
+    possible = checkIntermediatePieces(board, piece, pos)
+    if not possible:
+        return [False, 1]
+    # Take move.
+    origPiece = board[piece[0]][piece[1]]
+    board[piece[0]][piece[1]] = None
+    board[(pos[1]-1)][(5-pos[0])] = origPiece
+    # Check if move would capture piece.
+    if isinstance(possible, Piece):
+        return [True, board, possible]
+    else:
+        return [True, board, None]
+
+# Prints a text representation of the board to the console.
+# Purely for diagnostic purposes.
+def printBoard(board):
+    for line in x:
+        for piece in line:
+            if piece is None:
+                print("~", end=" ")
+            else:
+                print(piece, end=" ")
+        print()
+
 
 def main():
     x = makeBoard(1)
